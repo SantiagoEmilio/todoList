950f06d95c91ab8949a85d954fbf79363a70052c
Anonimo <anonimo@example.com>
2025-07-23 13:31:28 -0600

    se crea lo que es el header de la App

--- VIews/dasboarView.js
+++ VIews/dasboarView.js
@@ -1,10 +1,10 @@
+import { header } from "../components/header/headerComponent.js";
+
 export function dasboard(){
 
     let seccion = document.createElement('section');
 
-    let h1 = document.createElement('h1');
-    h1.innerText = "hola Mundo"
-    seccion.appendChild (h1);
+    seccion.appendChild(header());
 
     return seccion;
 
---
+++ components/header/headerComponent.js
@@ -0,0 +1,30 @@
+export function header() {
+    let header = document.createElement('header');
+    header.className = "header";
+
+    
+    let h1 = document.createElement('h1');
+    h1.innerText = "Todo-Iist";
+    header.appendChild(h1);
+
+    let divTarea = document.createElement('div');
+    divTarea.className = "header-tarea";
+
+    let pendientes = document.createElement('h1');
+    pendientes.innerText = "Pendientes";
+    divTarea.appendChild(pendientes);
+
+    let divContador = document.createElement('div');
+    divContador.className = "contador";
+    divContador.innerText = "0";
+    divTarea.appendChild(divContador);
+
+    let iconoUsuario = document.createElement('img');
+    iconoUsuario.src = "../assets/usuario.svg"; 
+    iconoUsuario.className = "icono-usuario";
+    divTarea.appendChild(iconoUsuario);
+
+    header.appendChild(divTarea);
+
+    return header;
+}
